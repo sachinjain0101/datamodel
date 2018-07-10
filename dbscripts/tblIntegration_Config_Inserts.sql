980b936dfaa05726668acfba3b9a8a00589b7331
Sachin Jain <sachin.jain@bullhorn.com>
2018-07-10 14:17:01 -0400

    Updated config entries

--- dbscripts/tblIntegration_Config_Inserts.sql
+++ dbscripts/tblIntegration_Config_Inserts.sql
@@ -1,12 +1,12 @@
 TRUNCATE TABLE TimeCurrent.dbo.tblIntegration_Config;
 
-INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('PNET_ASSIGNMENT_PROCESSOR_REST_URL','http://dev1app1/PeoplenetIntegration/api/Assignment');
-INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('AZURE_CONSUMER_QUEUE_NAME','sachin-new-test');
+INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('PNET_ASSIGNMENT_PROCESSOR_REST_URL','http://qa2-app2/PeoplenetIntegration/api/Assignment');
+INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('AZURE_CONSUMER_QUEUE_NAME','sachin-test-1');
 
-INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('AZURE_CONSUMER_EXECUTE_INTERVAL','5000');
-INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('DATA_SWAPPER_EXECUTE_INTERVAL','5000');
-INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('DATA_VALIDATOR_EXECUTE_INTERVAL','5000');
-INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('DATA_MAPPER_EXECUTE_INTERVAL','50000');
+INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('AZURE_CONSUMER_EXECUTE_INTERVAL','1000');
+INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('DATA_SWAPPER_EXECUTE_INTERVAL','1000');
+INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('DATA_VALIDATOR_EXECUTE_INTERVAL','1000');
+INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('DATA_MAPPER_EXECUTE_INTERVAL','10000');
 
 INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('AZURE_CONSUMER_THREADPOOL_SCHEDULER_TERMINATION_TIME_INSECONDS','180');
 INSERT INTO TimeCurrent.dbo.tblIntegration_Config (CfgKey,CfgValue) Values ('DATA_SWAPPER_THREADPOOL_SCHEDULER_TERMINATION_TIME_INSECONDS','180');
